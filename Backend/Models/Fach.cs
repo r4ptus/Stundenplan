ff547fb9928f7fc80ba85048614171ae754f1ede
r4ptus <kackb00n>
2018-05-29 10:12:37 +0200

    add models

---
+++ Backend/Models/Fach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Fach
+    {
+        string Name { get; set; }
+        string Raum { get; set; }
+        int Length { get; set; }
+        int Startzeit { get; set; }
+        string Info { get; set; }
+        bool FindetStatt { get; set; }
+    }
+}
